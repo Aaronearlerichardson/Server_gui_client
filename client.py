e4e487be07ed5c59ceee1d1007dbae1399e32f37
Aaronearlerichardson <jakdaxter31@gmail.com>
2021-12-17 19:01:43 -0500

    Added image to b64 function for server request formatting

--- client.py
+++ client.py
@@ -2,6 +2,17 @@ import requests
 import os
 from ecg_reader import preprocess_data
 from calculations import get_metrics
+import base64
+
+
+server = "http://127.0.0.1:5000"
+
+
+def image_to_b64(filename: str) -> str:
+    with open(filename, "rb") as image_file:
+        b64_bytes = base64.b64encode(image_file.read())
+    b64_string = str(b64_bytes, encoding="utf-8")
+    return b64_string
 
 
 if __name__ == "__main__":
@@ -12,6 +23,7 @@ if __name__ == "__main__":
                                fir_design="firwin")
     metrics = get_metrics(pre_data, rounding=4)
     patient1 = {"name": "Ann Ables", "id": 201, "hr": metrics["mean_hr_bpm"]}
-    r = requests.post("http://127.0.0.1:5000/new_patient", json=patient1)
+    patient1["image"] =
+    r = requests.post(server + "/new_patient", json=patient1)
     print(r.status_code)
     print(r.text)
